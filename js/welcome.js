90161a9902f832497f2c8447d33937f288a009ff
lizihengcz <admin@oigat.cn>
2023-08-24 12:26:43 +0800

    Site updated: 2023-08-24 12:26:38

--- js/welcome.js
+++ js/welcome.js
@@ -6,7 +6,7 @@ function welcome(){
         if(referrer.toUpperCase()==document.domain.toUpperCase())return;
     }
     swal({
-        title: "  欢迎！",
+        title: "  欢迎光临！  ",
         text: welcome_text+'\n打开页面左下方音乐以获得更佳体验！',
         imageUrl: "/img/logo.png",
         timer: 3000,
